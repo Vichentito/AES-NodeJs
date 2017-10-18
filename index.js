0ac35d10ed035bd79a4e0c82d3aef88f701320a1
Vichentito <chente54321@gmail.com>
2017-10-18 12:26:37 -0500

    Encriptacion
    ya casi queda

--- index.js
+++ index.js
@@ -1,13 +1,59 @@
 'use strict'
 const express = require('express')
-var app = require('express')
+var body_parser = require('body-parser')
+var app = express()
 var port = process.env.PORT || 9090
+var CryptoJS = require("crypto-js");
 
+app.use(body_parser.urlencoded({extended:true}));
 app.use(express.static('public'))
-server.listen(port, () => {
+
+
+app.listen(port, () => {
   console.log('Servidor iniciado en 8080')
 })
 
 app.get('/', function (req, res) {
-  //res.sendFile(__dirname + '/public/index.html');
-});
+  res.sendFile(__dirname + '/public/index.html');
+})
+
+app.post('/',(req,res)=>{
+  var txt = req.body.textToEncrypt
+  var psswrd = req.body.psswrd
+  var type = req.body.type
+  var proceso = req.body.type_cifer
+  proceso = (proceso == 1) ? "Decifrar" : "Cifrar"
+  var c = (proceso == 0) ? "cifrado" : "decifrado"
+  var encrypted
+  switch(type){
+      case 128:
+        if(txt.length == 16){
+           encrypted = CryptoJS.AES.encrypt(txt, psswrd);
+        }else{
+          encrypted = 'Tamaño de bloque incorrecto (tiene que ser de 128 bits = 16 caracteres)'
+        }
+      break
+      case 192:
+      break
+      case 256:
+      break
+      default:
+      break
+  }
+
+
+
+  var html = '<h1>Cifrado</h1>'
+  html+= '<h2>Datos enviados:</h2>'
+  html+= 'Texto a cifrar: ' + txt + '<br>'
+  html+= 'Contraseña: ' + psswrd + '<br>'
+  html+= 'Tipo : AES-' + type + '<br>'
+  html+= 'Proceso: ' + proceso + '<br>'
+  html+= 'Texto ' + c +': ' + '<br>'
+  html+= encrypted
+
+  res.send(html)
+})
+
+//var encrypted = CryptoJS.AES.encrypt("Message", "Secret Passphrase");
+//var decrypted = CryptoJS.AES.decrypt(encrypted, "Secret Passphrase");
